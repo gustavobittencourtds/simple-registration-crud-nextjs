33da3283c62b8f1fc3fb96c231de9ef81598f1ac
Gustavo Bittencourt <gustavo.bittencourtds@gmail.com>
2023-12-05 19:43:20 -0300

    testing workflow

--- jest.config.mjs
+++ jest.config.mjs
@@ -6,6 +6,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["core-js"],
+  
 };
 
 
